82331bc023dc6a468a364805e3c721036d6f8b65
Carlos <Carlos.admcode@gmail.com>
2021-08-22 17:19:41 -0300

    Cria classe Page
    Esta classe e responsavel pela montagem dos templates das páginas
    Apartir de rotas predefinidas.

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php 
 
-require_once("vendor/autoload.php");
+	require_once("vendor/autoload.php");
+	use \Slim\Slim;
+	use \Hcode\Page;
 
-$app = new \Slim\Slim();
+	$app = new Slim();
 
-$app->config('debug', true);
+	$app->config('debug', true);
 
-$app->get('/', function() {
-    
-	$sql = new Hcode\DB\Sql();
-	$results = $sql->select("SELECT * FROM tb_users");
+	$app->get('/', function() {
+		//bucha dados e cabeçalho da página
+		$page = new Page();
+		$page->setTpl("index");
+	});
 
-	echo json_encode($results);
-});
+	$app->run();
 
-$app->run();
-
- ?>
+?>
